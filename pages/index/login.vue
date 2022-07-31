7754ca4dcae87c2c272d9a4ac315206a55b03c42
xayrullo <xayrullo.jaloldinov@gmail.com>
2022-08-01 01:25:54 +0500

    login connected

--- pages/index/login.vue
+++ pages/index/login.vue
@@ -45,10 +45,22 @@
                   type="text"
                   autocomplete="text"
                   v-model="auth.identifier"
-                  placeholder="Phone number"
+                  :placeholder="$t('email-or-phone')"
                   :state="errors[0] ? false : valid ? true : null"
                   required
-                  class="focus:outline-none appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 sm:text-sm"
+                  class="
+                    focus:outline-none
+                    appearance-none
+                    block
+                    w-full
+                    px-3
+                    py-2
+                    border border-gray-300
+                    rounded-md
+                    shadow-sm
+                    placeholder-gray-400
+                    sm:text-sm
+                  "
                   :class="
                     errors.length > 0
                       ? 'border-red-400'
@@ -57,6 +69,48 @@
                       : 'border-gray-300'
                   "
                 />
+                <div class="text-red-500 text-xs">{{ errors[0] }}</div>
+              </ValidationProvider>
+            </div>
+            <div v-if="isPhone" class="mt-1">
+              <label for="otp" class="block mb-1 text-sm font-medium text-gray-700">
+                {{ $t('enter-confirm-code') }}</label
+              >
+              <ValidationProvider
+                v-slot="{ errors }"
+                name="otp"
+                rules="required|min:4|max:4"
+                mode="eager"
+              >
+                <input
+                  name="otp"
+                  type="text"
+                  autocomplete="text"
+                  v-model="otp"
+                  :placeholder="$t('enter-confirm-code')"
+                  required
+                  class="
+                    focus:outline-none
+                    appearance-none
+                    block
+                    w-full
+                    px-3
+                    py-2
+                    border
+                    rounded-md
+                    shadow-sm
+                    placeholder-gray-400
+                    sm:text-sm
+                  "
+                  :class="
+                    errors.length > 0
+                      ? 'border-red-400'
+                      : otp
+                      ? 'border-green-600'
+                      : 'border-gray-300'
+                  "
+                />
+                <div class="text-red-500 text-xs">{{ errors[0] }}</div>
               </ValidationProvider>
             </div>
             <div class="mt-1" v-if="isEmail">
@@ -68,7 +122,19 @@
                   :placeholder="$t('password')"
                   :state="errors[0] ? false : valid ? true : null"
                   required
-                  class="focus:outline-none appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 sm:text-sm"
+                  class="
+                    focus:outline-none
+                    appearance-none
+                    block
+                    w-full
+                    px-3
+                    py-2
+                    border border-gray-300
+                    rounded-md
+                    shadow-sm
+                    placeholder-gray-400
+                    sm:text-sm
+                  "
                   :class="
                     errors.length > 0
                       ? 'border-red-400'
@@ -77,13 +143,29 @@
                       : 'border-gray-300'
                   "
                 />
+                <div class="text-red-500 text-xs">{{ errors[0] }}</div>
               </ValidationProvider>
             </div>
+            <div class="text-red-500 text-xs">{{ authError }}</div>
             <button
               :class="invalid ? 'bg-gray-300' : 'bg-green-600 hover:bg-green-700 text-white'"
               :disabled="invalid"
               type="submit"
-              class="mt-3 w-full flex justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white focus:outline-none"
+              class="
+                mt-3
+                w-full
+                flex
+                justify-center
+                px-4
+                py-2
+                border border-transparent
+                rounded-md
+                shadow-sm
+                text-sm
+                font-medium
+                text-white
+                focus:outline-none
+              "
             >
               {{ $t('sign-in') }}
               <vue-loaders v-if="loading" name="ball-beat" color="white" scale="0.5" />
@@ -122,20 +204,45 @@ export default {
       isAuthError: false,
       loading: false,
       isEmail: false,
+      isPhone: false,
+      otp: '',
     }
   },
   watch: {
     'auth.identifier'() {
       const EMAILREG =
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      const MOBILEREG = /^([+]?[9]{2}[8][0-9]{2}[0-9]{7})$/
       if (EMAILREG.test(this.auth.identifier)) {
+        this.isPhone = false
         this.isEmail = true
+      } else if (MOBILEREG.test(this.auth.identifier)) {
+        this.sendOtp()
       } else {
         this.isEmail = false
+        this.isPhone = false
+      }
+    },
+    otp() {
+      if (this.otp.length === 4) {
+        this.registerPhone()
       }
     },
   },
   methods: {
+    sendOtp() {
+      this.$axios
+        .$post('/users-permissions/send_otp', { phone: this.auth.identifier })
+        .then((response) => {
+          this.isPhone = true
+          this.isEmail = false
+        })
+        .catch((error) => {
+          this.isPhone = false
+          this.isEmail = false
+          this.authError = error.response.data.message
+        })
+    },
     async tryToLogIn() {
       if (this.auth.identifier.includes('+') > 0) {
         this.auth.identifier = this.auth.identifier.substring(1)
@@ -143,47 +250,72 @@ export default {
       this.authError = ''
       this.loading = true
       if (this.isEmail) {
-        this.$snotify.info('Logging in...')
-        try {
-          await this.$auth
-            .loginWith('local', {
-              data: this.auth,
-            })
-            .then(async (res) => {
-              await this.$store
-                .dispatch('getUsers', {
-                  link: '/users/me',
-                  query: {
-                    populate: '*',
-                  },
-                })
-                .then((response) => {
-                  localStorage.setItem('user_info', JSON.stringify(response.data))
-                })
-              await this.$bridge.$emit('join_chat', {
-                username: res.data.user.username,
-                user_id: res.data.user.id,
-              })
-              this.loading = false
-              await this.$snotify.success('Successfully Logged In')
-              this.onClose()
-            })
-        } catch (e) {
-          if (e.response) this.authError = e.response.data.error.message
-          this.loading = false
-        }
+        this.registerEmail()
       } else {
+        this.registerPhone()
+      }
+    },
+    async registerEmail() {
+      this.$snotify.info('Logging in...')
+      try {
+        await this.$auth
+          .loginWith('local', {
+            data: this.auth,
+          })
+          .then(async (res) => {
+            await this.$store
+              .dispatch('getUsers', {
+                link: '/users/me',
+                query: {
+                  populate: '*',
+                },
+              })
+              .then((response) => {
+                localStorage.setItem('user_info', JSON.stringify(response.data))
+              })
+            await this.$bridge.$emit('join_chat', {
+              username: res.data.user.username,
+              user_id: res.data.user.id,
+            })
+            this.loading = false
+            this.$router.push(this.localePath('/'))
+            await this.$snotify.success('Successfully Logged In')
+          })
+      } catch (e) {
+        if (e.response) this.authError = e.response.data.error.message
+        this.loading = false
+      }
+    },
+    registerPhone() {
+      this.$snotify.info('Logging in...')
+      try {
         this.$axios
-          .post('/users-permissions/send_otp', { phone: this.auth.identifier })
-          .then(() => {
+          .$post('/users-permissions/login_otp', { phone: this.auth.identifier, otp: this.otp })
+          .then(async (res) => {
+            console.log('User res', res)
+            this.$auth.setUser(res.user)
+            this.$auth.setUserToken(res.jwt)
+            await this.$store
+              .dispatch('getUsers', {
+                link: '/users/me',
+                query: {
+                  populate: '*',
+                },
+              })
+              .then((response) => {
+                localStorage.setItem('user_info', JSON.stringify(response.data))
+              })
+            await this.$bridge.$emit('join_chat', {
+              username: res.user.username,
+              user_id: res.user.id,
+            })
             this.loading = false
-            this.confirmCode({ username: this.auth.identifier, isLogin: true, isOtpSuccess: null })
+            this.$router.push(this.localePath('/'))
+            await this.$snotify.success('Successfully Logged In')
           })
-          .catch((e) => {
-            this.openSignUp()
-            this.authError = e.response.data.error.message
-            this.loading = false
-          })
+      } catch (e) {
+        if (e.response) this.authError = e.response.data.error.message
+        this.loading = false
       }
     },
   },
