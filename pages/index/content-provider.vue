ca09f84d385b2f74b1a019f385b5979517151aac
xayrullo <xayrullo.jaloldinov@gmail.com>
2022-07-31 23:45:33 +0500

    register as content provider done

--- pages/index/content-provider.vue
+++ pages/index/content-provider.vue
@@ -26,12 +26,25 @@
                     type="text"
                     name="organization_name"
                     id="organization_name"
-                    v-model="form.organization_name"
-                    class="focus:outline-none appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 sm:text-sm"
+                    v-model="company.name"
+                    :disabled="!isMainRegister"
+                    class="
+                      focus:outline-none
+                      appearance-none
+                      block
+                      w-full
+                      px-3
+                      py-2
+                      border
+                      rounded-md
+                      shadow-sm
+                      placeholder-gray-400
+                      sm:text-sm
+                    "
                     :class="
                       errors.length > 0
                         ? 'border-red-400'
-                        : form.organization_name
+                        : company.name
                         ? 'border-green-600'
                         : 'border-gray-300'
                     "
@@ -46,35 +59,22 @@
                   type="text"
                   name="position"
                   id="position"
-                  v-model="form.position"
-                  class="focus:outline-none appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 sm:text-sm"
+                  v-model="company.position"
+                  :disabled="!isMainRegister"
+                  class="
+                    focus:outline-none
+                    appearance-none
+                    block
+                    w-full
+                    px-3
+                    py-2
+                    border border-gray-300
+                    rounded-md
+                    shadow-sm
+                    placeholder-gray-400
+                    sm:text-sm
+                  "
                 />
-              </div>
-              <div class="md:col-span-1 col-span-2">
-                <label for="name" class="block mb-1 text-sm font-medium text-gray-700"
-                  >{{ $t('contact-name') }}*</label
-                >
-                <ValidationProvider
-                  v-slot="{ errors }"
-                  name="contact_name"
-                  rules="required"
-                  mode="eager"
-                >
-                  <input
-                    type="text"
-                    name="contact_name"
-                    id="contact_name"
-                    v-model="form.contact_name"
-                    class="focus:outline-none appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 sm:text-sm"
-                    :class="
-                      errors.length > 0
-                        ? 'border-red-400'
-                        : form.contact_namee
-                        ? 'border-green-600'
-                        : 'border-gray-300'
-                    "
-                  />
-                </ValidationProvider>
               </div>
               <div class="md:col-span-1 col-span-2">
                 <label for="phone" class="block mb-1 text-sm font-medium text-gray-700">{{
@@ -90,20 +90,33 @@
                     type="text"
                     name="phone"
                     id="phone"
-                    v-model="form.phone"
-                    v-mask="'+998## ###-##-##'"
-                    class="focus:outline-none appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 sm:text-sm"
+                    v-model="company.phone"
+                    :disabled="!isMainRegister"
+                    v-mask="'+##### ###-##-##'"
+                    class="
+                      focus:outline-none
+                      appearance-none
+                      block
+                      w-full
+                      px-3
+                      py-2
+                      border
+                      rounded-md
+                      shadow-sm
+                      placeholder-gray-400
+                      sm:text-sm
+                    "
                     :class="
                       errors.length > 0
                         ? 'border-red-400'
-                        : form.phone
+                        : company.phone
                         ? 'border-green-600'
                         : 'border-gray-300'
                     "
                   />
                 </ValidationProvider>
               </div>
-              <div class="col-span-2">
+              <div class="md:col-span-1 col-span-2">
                 <label for="email" class="block mb-1 text-sm font-medium text-gray-700">{{
                   $t('email')
                 }}</label>
@@ -111,8 +124,21 @@
                   type="email"
                   name="email"
                   id="email"
-                  v-model="form.email"
-                  class="focus:outline-none appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 sm:text-sm"
+                  v-model="company.email"
+                  :disabled="!isMainRegister"
+                  class="
+                    focus:outline-none
+                    appearance-none
+                    block
+                    w-full
+                    px-3
+                    py-2
+                    border border-gray-300
+                    rounded-md
+                    shadow-sm
+                    placeholder-gray-400
+                    sm:text-sm
+                  "
                 />
               </div>
               <div class="col-span-2">
@@ -124,13 +150,26 @@
                   name="summary"
                   id="summary"
                   rows="4"
-                  v-model="form.short_summary"
+                  v-model="company.shortinfo"
+                  :disabled="!isMainRegister"
                   :placeholder="
                     $t(
                       'short-summary-of-agricultural-environmental-experience-and-activities-of-organization-in-Karakalpakstan'
                     )
                   "
-                  class="focus:outline-none appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 sm:text-sm"
+                  class="
+                    focus:outline-none
+                    appearance-none
+                    block
+                    w-full
+                    px-3
+                    py-2
+                    border border-gray-300
+                    rounded-md
+                    shadow-sm
+                    placeholder-gray-400
+                    sm:text-sm
+                  "
                 />
               </div>
               <div class="col-span-2">
@@ -142,29 +181,47 @@
                   name="description"
                   id="description"
                   rows="4"
-                  v-model="form.short_description"
+                  v-model="company.fullinfo"
+                  :disabled="!isMainRegister"
                   :placeholder="
                     $t(
                       'short-description-of-organization-size-relationship-to-agricultural-environmental-sectors-in-Karakalpakstan'
                     )
                   "
-                  class="focus:outline-none appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 sm:text-sm"
+                  class="
+                    focus:outline-none
+                    appearance-none
+                    block
+                    w-full
+                    px-3
+                    py-2
+                    border border-gray-300
+                    rounded-md
+                    shadow-sm
+                    placeholder-gray-400
+                    sm:text-sm
+                  "
                 />
               </div>
-              <div class="flex items-center mt-2 col-span-2">
-                <input
-                  name="termsOfUse"
-                  type="checkbox"
-                  :value="true"
-                  class="h-5 w-5 text-green-600 focus:ring-green-600 border-gray-300 rounded"
-                />
-                <label for="termsOfUse" class="ml-2 block text-gray-600 text-base">
-                  {{ $t('i-agree-with') }}
-                  <span class="text-green-600 border-b border-green-600">{{
-                    $t('terms-of-use')
-                  }}</span>
-                </label>
-              </div>
+              <ValidationProvider name="checked" rules="checked" mode="eager" v-slot="{ errors }">
+                <div class="flex items-center mt-2 col-span-2">
+                  <input
+                    name="termsOfUse"
+                    type="checkbox"
+                    v-model="isAgree"
+                    :value="true"
+                    :disabled="!isMainRegister"
+                    class="h-5 w-5 text-green-600 focus:ring-green-600 border-gray-300 rounded"
+                  />
+                  <label for="termsOfUse" class="ml-2 block text-gray-600 text-base">
+                    {{ $t('i-agree-with') }}
+                    <span class="text-green-600 border-b border-green-600">
+                      {{ $t('terms-of-use') }}
+                    </span>
+                  </label>
+                </div>
+                <div class="text-red-500 text-xs">{{ errors[0] }}</div>
+              </ValidationProvider>
               <button
                 :class="invalid ? 'bg-gray-300' : 'bg-green-600 hover:bg-green-700 text-white'"
                 :disabled="invalid"
@@ -182,22 +239,111 @@
 
 <script>
 import background from '/assets/images/background.png'
+import axios from 'axios'
 export default {
   name: 'UserInterprise',
   auth: false,
   data() {
     return {
       image: background,
-      form: {
-        organization_name: '',
-        position: '',
-        contact_name: '',
+      company: {
+        name: '',
+        tin: '',
         phone: '',
         email: '',
-        short_summary: '',
-        short_description: '',
+        activitytypes: [],
+        agrocultureareas: [],
+        users: [],
+        district: null,
+        shortinfo: '',
+        fullinfo: '',
+        position: '',
       },
+      phone: '',
+      auth: {
+        identifier: 'zokirjonov@gmail.com',
+        password: '123456',
+      },
+      jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzMsImlhdCI6MTY1OTI5Mjc3MywiZXhwIjoxNjYxODg0NzczfQ.veRGIxE_8coEnDIZnJXuUfBoK4J2HZDJNCR6BKgLCVo',
+      isMainRegister: false,
+      isAgree: false,
     }
+  },
+  mounted() {},
+  watch: {
+    phone: {
+      handler() {
+        if (this.phone) this.company.phone = this.phone.replace(/[^0-9]/g, '')
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    mediaChange({ target }, type) {
+      const formData = new FormData()
+      formData.append('files', target.files[0])
+      axios({
+        baseURL: process.env.VUE_APP_BASE_URL,
+        url: `/upload`,
+        method: 'POST',
+        data: formData,
+        headers: {
+          Authorization: `Bearer ${this.jwt}`,
+        },
+      }).then((res) => {
+        if (type === 'resume') {
+          this.additional.resume_url = res.data[0].url.substring(8)
+          return
+        }
+      })
+    },
+    onSubmit() {
+      axios({
+        baseURL: process.env.VUE_APP_BASE_URL,
+        url: `/companies`,
+        method: 'POST',
+        data: { data: this.company },
+        headers: {
+          Authorization: `Bearer ${this.jwt}`,
+        },
+      }).then(async (res) => {
+        try {
+          await this.$auth
+            .loginWith('local', {
+              data: this.auth,
+            })
+            .then(async (res) => {
+              await this.$store
+                .dispatch('getUsers', {
+                  link: '/users/me',
+                  query: {
+                    populate: '*',
+                  },
+                })
+                .then((response) => {
+                  localStorage.setItem('user_info', JSON.stringify(response.data))
+                })
+              await this.$bridge.$emit('join_chat', {
+                username: res.data.user.username,
+                user_id: res.data.user.id,
+              })
+              this.loading = false
+              await this.$snotify.success('Successfully Logged In')
+              this.$router.push(this.localePath('/'))
+            })
+        } catch (e) {
+          if (e.response) this.authError = e.response.data.error.message
+          this.loading = false
+        }
+      })
+    },
+    mainRegisterSuccess(e) {
+      this.isMainRegister = e.isSuccess
+      this.auth.identifier = e.user.username
+      this.auth.password = e.password
+      this.company.users.push(e.user.id)
+      this.jwt = e.jwt
+    },
   },
 }
 </script>
