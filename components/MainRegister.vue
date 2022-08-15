fd979f23ba87e4e91d9bdc5a30107a1a7f5d08f9
xayrullo <xayrullo.jaloldinov@gmail.com>
2022-08-15 15:20:25 +0500

    some additionals added

--- components/MainRegister.vue
+++ components/MainRegister.vue
@@ -409,7 +409,7 @@
             :class="
               invalid
                 ? 'text-gray-700 bg-gray-100 hover:bg-gray-200 border-gray-100 hover:border-gray-200 focus:ring-gray-500'
-                : 'text-green-700 bg-green-100 hover:bg-green-200 border-green-100 hover:border-green-200 focus:ring-green-500'
+                : 'text-white focus:outline-none text-sm bg-green-700 p-3'
             "
             :disabled="invalid"
           >
