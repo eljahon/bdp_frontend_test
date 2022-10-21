070cb63aae263d3f4e2e12ff5cab3580ffb988ac
BekzodRakhmonjonov <bekzod03233@mail.ru>
2022-10-21 18:26:12 +0500

    feat: add lang to colsultant carsd

--- components/Experts.vue
+++ components/Experts.vue
@@ -32,7 +32,7 @@
 <!--          {{ data.consultantcategory.name }}-->
 <!--        </div>-->
         <div class='group-hover:flex items-center hidden'>
-          <span class='text-gray-500 text-xs'>Rating: &nbsp;</span>
+          <span class='text-gray-500 text-xs'>{{ $t('rating') }}: &nbsp;</span>
           <div class="bh-stars" data-bh-rating="4.9">
             <svg version="1.1" class="bh-star bh-star--1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" xml:space="preserve"><path class="outline" d="M12,4.2L14.5,9l0.2,0.5l0.5,0.1l5.5,0.8L16.8,14l-0.4,0.4l0.1,0.5l1,5.3l-5-2.5L12,17.5l-0.4,0.2l-5,2.5L7.5,15l0.1-0.5 L7.2,14l-4-3.7l5.5-0.8l0.5-0.1L9.5,9L12,4.2 M11.9,2L8.6,8.6L1,9.7l5.5,5.1L5.2,22l6.8-3.4l6.8,3.4l-1.3-7.2L23,9.6l-7.6-1L11.9,2 L11.9,2z"/><polygon class="full" points="18.8,22 12,18.6 5.2,22 6.5,14.8 1,9.7 8.6,8.6 11.9,2 15.4,8.6 23,9.6 17.5,14.7"/><polyline class="left-half" points="12,18.6 5.2,22 6.5,14.8 1,9.7 8.6,8.6 11.9,2"/></svg>
 
@@ -56,7 +56,7 @@
             my-1
           "
         >
-          <span>Degree: &nbsp;</span> {{data.additionalinfo.last_degree }}
+          <span>{{ $t('degree') }}: &nbsp;</span> {{data.additionalinfo.last_degree }}
         </div>
         <div
           v-if='data.additionalinfo && data.additionalinfo.otherarea'
@@ -68,7 +68,7 @@
             my-1
           "
         >
-          <span>Area of agriculture: &nbsp;</span> {{data.additionalinfo.otherarea }}
+          <span>{{ $t('area-of-agriculture') }}: &nbsp;</span> {{data.additionalinfo.otherarea }}
         </div>
         <div
           v-if='data.additionalinfo && data.additionalinfo.env_otherarea'
@@ -81,7 +81,7 @@
             my-1
           "
         >
-          <span>Area of consultancy: &nbsp;</span> {{data.additionalinfo.env_otherarea }}
+          <span>{{ $t('area-of-consultancy') }}: &nbsp;</span> {{data.additionalinfo.env_otherarea }}
         </div>
         <button
           v-if="!isConsultant"
